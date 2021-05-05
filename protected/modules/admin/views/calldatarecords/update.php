02f7e98acc8a4a6cc6426cff1b2b6df9774f4b06
swmayur <mayur@scrumwheel.com>
2021-05-05 19:30:00 +0530

    Feature:CRUD created for company

---
+++ protected/modules/admin/views/calldatarecords/update.php
@@ -0,0 +1,6 @@
+<?php
+/* @var $this CalldatarecordsController */
+/* @var $model OrganizationInfo */
+$this->pageTitle = 'Update Organization';
+?>
+<?php $this->renderPartial('_form', array('model'=>$model)); ?>
